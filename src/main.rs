e1298dd021b84dcd6b3a2ea13c7e3739ccef84e7
Matdrox <Matdrox@users.noreply.github.com>
2022-12-18 12:18:31 +0100

    [Updated] Cargo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// cargo check
+// cargo build
+// rustc main.rs
+// ./main.exe
+
+fn add(x: i32, y: i32) -> i32 {
+	x + y
+}
+
 fn main() {
-	println!("Hello, World!");
+    // println!("Hello, World!"); // Used '!' because it's a marco. Macros generate code at compile-time rather than calling a function
+	let result = add(5, 3);
+	println!("The result is {}", result);
 }
